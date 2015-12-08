f7207b0f090bda229ab73c5f77be4ee937375209
Juan Ignacio Hita Manso <alu0100713213@ull.edu.es>
2015-12-08 17:35:05 +0000

    Primeras pruebas corriendo

--- spec/Bibliografia_spec.rb
+++ spec/Bibliografia_spec.rb
@@ -2,12 +2,33 @@ require 'spec_helper'
 require 'Bibliografia'
 
 describe Bibliografia do
-	before :all do 
-        @l1 = Bibliog.new(['Dave','Andy','Chad'], ['Thomas', 'Hunt', 'Fowler'], 'Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide', 'Pragmatic Bookshelf', 4, 'July 7', 2013, ['9781937785499', '1937785491'], 'The Facets of Ruby')
-        @l2 = Bibliog.new(['Dave','Andy','Chad'], ['Thomas', 'Hunt', 'Fowler'], 'Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide', 'Pragmatic Bookshelf', 4, 'July 7', 2013, ['9781937785499', '1937785491'])
-        @l3 = Bibliog.new(['Dave','Andy','Chad'], ['Thomas', 'Hunt', 'Fowler'], 'Titulo2', 'Pragmatic Bookshelf', 4, 'July 7', 2013, ['9781937785499', '1937785491'], 'The Facets of Ruby')
-        @l4 = Bibliog.new(['Dave','Andy','Chad'], ['Thomas', 'Hunt', 'Fowler'], 'Titulo2', 'Editorial2', 4, 'July 7', 2013, ['9781937785499', '1937785491'], 'The Facets of Ruby')
-    end
+	before :all do
 
+	@l1 = Bibliografia::Libro.new(['Dave','Andy','Chad'], ['Thomas', 'Hunt', 'Fowler'], 'Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide', '2323', 'Pragmatic Bookshelf', 'edicion 2', 4, 'July 7', 2013, ['9781937785499', '1937785491'])
+	end
+	
+	describe "#PRUEBAS DE LOS LIBROS" do
+		it "Debe existir un titulo" do
+		 expect(@l1.titulo).to eq('Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide')
+		end
+		it "Debe existir una serie" do
+		     expect(@l1.serie).to eq('2323')
+		end
+		it "Debe existir una editorial" do
+		     expect(@l1.num_editorial).to eq('Pragmatic Bookshelf')
+		end	
+		it "Debe existir un día" do
+		     expect(@l1.dia).to eq(4)
+		end
+		it "Debe existir un mes" do
+		     expect(@l1.mes).to eq('July 7')
+		end
+		it "Debe existir un año" do
+		     expect(@l1.ano).to eq(2013)
+		end
+	end
 end
+
+
+
 
