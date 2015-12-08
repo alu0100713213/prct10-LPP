c55fac956d423984ab8509ef2d32b13723e96036
Juan Ignacio Hita Manso <alu0100713213@ull.edu.es>
2015-12-08 18:39:53 +0000

    Añadiendo citas de la APA

--- lib/Bibliografia.rb
+++ lib/Bibliografia.rb
@@ -1,6 +1,8 @@
 require "Bibliografia/version"
 require "Bibliografia/Bibliografia.rb"
 require "Bibliografia/lista.rb"
+require "Bibliografia/Citas_APA.rb"
+
 module Bibliografia
   # Your code goes here...
 end
